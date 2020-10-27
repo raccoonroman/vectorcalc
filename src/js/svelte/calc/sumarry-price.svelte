09e23b47735b6d74f436bc886011c4f5a00ba022
fronir <fromanyr@gmail.com>
2020-10-27 15:16:46 +0200

    add slide transition for summary-price

--- src/js/svelte/calc/sumarry-price.svelte
+++ src/js/svelte/calc/sumarry-price.svelte
@@ -1,8 +1,9 @@
 <script>
+  import { slide } from 'svelte/transition';
   export let totalPrice;
 </script>
 
-<div class="calc__summary">
+<div class="calc__summary" transition:slide>
   <div class="calc__summary-price">
     <p class="calc__summary-price-label">Орієнтовна вартість</p>
     <p class="calc__summary-price-value">{totalPrice} грн</p>
